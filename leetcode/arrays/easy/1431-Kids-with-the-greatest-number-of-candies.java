e0957006f57f37f919765e4fc85943408056de29
Fahad <fahadmasood03@gmail.com>
2022-04-03 12:39:52 +0530

    modify code

--- leetcode/arrays/easy/1431-Kids-with-the-greatest-number-of-candies.java
+++ leetcode/arrays/easy/1431-Kids-with-the-greatest-number-of-candies.java
@@ -2,19 +2,20 @@ class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List kidsWithCandies = new ArrayList();
 
-        int max = 0;
+        int max = candies[0];
 
-        for (int i = 0; i < candies.length; i++) {
+        for (int i = 1; i < candies.length; i++) {
             if (candies[i] > max) {
                 max = candies[i];
             }
         }
 
         for (int i = 0; i < candies.length; i++) {
-            if (candies[i] + extraCandies >= max)
+            if (candies[i] + extraCandies >= max) {
                 kidsWithCandies.add(true);
-            else
+            } else {
                 kidsWithCandies.add(false);
+            }
         }
         return kidsWithCandies;
     }
