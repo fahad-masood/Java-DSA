e8e38feacc38c4b9a5ef647d1e6b6a1b723b1f62
Fahad <fahadmasood03@gmail.com>
2022-04-03 13:19:28 +0530

    add code

---
+++ leetcode/arrays/easy/1365-Numbers-Smaller-Than-Current-Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j] && j != i) {
+                    ans[i]++;
+                }
+            }
+        }
+        return ans;
+    }
+}
