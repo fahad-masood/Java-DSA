b978eb47d657e23754f816ca2aedce994b788b7b
Fahad <fahadmasood03@gmail.com>
2022-05-12 16:19:34 +0530

    add code leetcode#645

---
+++ leetcode/sorting/easy/645-set-mismatch.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/set-mismatch/
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i = 0; 
+        while(i < nums.length){
+            int correctIndex = nums[i] - 1;
+            if(nums[i] != nums[correctIndex]){
+                // swap
+                int temp = nums[correctIndex];                    
+                nums[correctIndex] = nums[i];
+                nums[i] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j+1){
+                return new int[] {nums[j], j+1};
+            }
+        }    
+        return new int[]{-1,-1};
+    }
+}
